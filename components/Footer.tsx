43d43cbdc55559b89499e2eb1836d1911caee833
alifsyahakbar <alifsyah789@gmail.com>
2023-01-15 02:32:22 +0700

    remove image footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,6 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div>
-      <Image
-        priority
-        src={"/assets/footer.png"}
-        unoptimized={false}
-        alt="footer"
-        width={100}
-        height={100}
-        className="fixed bottom-0 -right-6 lg:w-40 w-32 h-auto"
-      ></Image>
       <div className="flex-1 text-center m-4 p-2 text-slate-800 dark:text-white">
         <h2 className="font-bangers text-lg lg:text-3xl">Moshidesu.Moe</h2>
         <a href="https://github.com/alifsyahakbar">
